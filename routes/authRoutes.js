5b1fcf32c724168e7786a6fff655f45c29a8166b
tiagooo0 <thurstaltamirano@escuelasproa.edu.ar>
2024-07-24 21:51:57 -0300

    agrego cositas en las vistas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,22 @@ const router = express.Router();
 const passport = require('passport');
 
 // Ruta para el login
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/createpost', // Redirige a la página para crear un post
-    failureRedirect: '/login',
-    failureFlash: true // Asegúrate de haber configurado connect-flash para manejar mensajes de error
-}));
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) { return next(err); }
+        if (!user) { 
+            return res.render('login', { error: 'Nombre de usuario o contraseña incorrectos' });
+        }
+        req.logIn(user, (err) => {
+            if (err) { return next(err); }
+            return res.redirect('/createpost'); // Redirige a la página para crear un post
+        });
+    })(req, res, next);
+});
+
+// Ruta para mostrar la página de login
+router.get('/login', (req, res) => {
+    res.render('login'); // Puedes agregar un mensaje de error aquí si es necesario
+});
 
 module.exports = router;
