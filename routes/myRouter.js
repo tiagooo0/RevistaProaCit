641bea623c38918cf4f6031068f41e4aa92193f7
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2024-11-26 13:07:01 -0300

    Add files via upload

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -15,8 +15,7 @@ router.post('/create', isAuthenticated, postController.createPost);
 
 // Mostrar posts por categoría
 router.get('/category/:category', postController.postsByCategory);
-// Agregar un comentario a un post
-router.post('/post/:id/comments', postController.addComment);
+
 // Mostrar un post individual
 router.get('/post/:id', postController.getPost);
 
